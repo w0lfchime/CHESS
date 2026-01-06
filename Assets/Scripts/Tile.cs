3fb283884b8de5972c32b0bed4e3f88005951a31
frogggggggg <139102704+frogggggggg@users.noreply.github.com>
2026-01-05 20:03:37 -0500

    e

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,196 +1,179 @@
-using System.Collections.Generic;
-using UnityEngine;
-using System.Linq;
-using System.Collections;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
-[System.Serializable]
+[Serializable]
 public class nameMaterialPair
 {
     public string key;
     public Material value;
 }
+
 public class Tile : MonoBehaviour
 {
-	public List<nameMaterialPair> effectDictionary = new List<nameMaterialPair>();
+    public List<nameMaterialPair> effectDictionary = new();
 
-	public int TileBoardX, TileBoardY;
-	public Renderer rend;
+    public int TileBoardX, TileBoardY;
+    public Renderer rend;
 
-	public List<ChessPiece> tileOccupants = new List<ChessPiece>();
-	public Dictionary<string, int> effects = new Dictionary<string, int>();
+    public List<ChessPiece> tileOccupants = new();
+    public Dictionary<string, int> effects = new();
 
-	[Flags]
-	public enum conditions
-	{
-		None    = 0,
-		Black   = 1 << 0,
-		White     = 1 << 1,
-		All   = ~0
-	}
-	public conditions obstructed;
-	public bool isWhite;
-	private Coroutine raise;
-
-	void Awake() => rend = GetComponentInChildren<Renderer>();
-
-	public IEnumerable<ChessPiece> GetAllOccupants()
-	{
-		foreach (var o in tileOccupants) yield return o;
-	}
-
-	public void SwapColor(bool white)
-	{
-		rend.sharedMaterial = white ? ChessBoard2.Instance.WhiteTileMat : ChessBoard2.Instance.BlackTileMat;
-		isWhite = white;
-	}
-
-	public void AddPiece(ChessPiece piece)
-	{
-		tileOccupants.Add(piece);
-		piece.ScaleLongestAxis();
-		UpdatePieces();
-		UpdateEffects();
-	}
-
-	public void AddEffect(string name, int duration, float distance = 0)
-	{
-		if (effects.ContainsKey(name))
-		{
-			if (duration > effects[name]) effects[name] = duration;
-		}
-		else 
-		{
-			effects.Add(name, duration); 
-		}
-
-		UpdateEffects(false, distance);
-
-		StartCoroutine(EffectRaise(-.3f, distance, effects.Count));
-	}
-
-	public void RemovePiece(ChessPiece piece)
-	{
-		if (piece == null) return;
-		tileOccupants.Remove(piece);
-
-		UpdatePieces();
-	}
-
-	public void UpdatePieces()
-	{
-		foreach (ChessPiece p in tileOccupants)
-		{
-			p.UpdatePieceData(this);
-		}
-
-	}
-
-	public void UpdateEffects(bool turnPass = true, float distance = 0)
-	{
-		Material[] currentMaterials = rend.materials;
-		Material[] newMaterialsArray = new Material[effects.Count + 1];
-		newMaterialsArray[0] = currentMaterials[0];
-
-		int count = 1;
-		List<string> keysToModify = new List<string>();
-		foreach (string name in effects.Keys)
-		{
-			if (turnPass) keysToModify.Add(name);
-		}
-
-		foreach (string name in keysToModify)
-		{
-			effects[name] -= 1;
-			if (effects[name] <= 0)
-			{
-				effects.Remove(name);
-
-				Material mat = rend.materials[count];
-				newMaterialsArray[count] = mat;
-				StartCoroutine(EffectRaise(0, distance,count));
-				count++;
-			}
-		}
-
-		foreach (string name in effects.Keys) // set materials for each
-		{
-			Material mat = (rend.materials.Length > count) ? rend.materials[count] : effectDictionary.FirstOrDefault(kvp => kvp.key == name).value;
-			newMaterialsArray[count] = mat;
-			count++;
-		}
-
-		rend.materials = newMaterialsArray;
-
-
-		if (tileOccupants.Count > 0 || effects.ContainsKey("water"))
-			obstructed |= conditions.All;
-
-		if (effects.ContainsKey("scarewhite"))
-			obstructed |= conditions.White;
-
-		if (effects.ContainsKey("scareblack"))
-			obstructed |= conditions.Black;
-	}
-
-	public void Highlight(float distance)
-	{
-		if(raise!=null) StopCoroutine(raise);
-		raise = StartCoroutine(HighlightRaise(-.5f, distance));
-
-	}
-
-	public void UnHighlight(float distance)
-	{
-		if (raise != null) StopCoroutine(raise);
-		raise = StartCoroutine(HighlightRaise(0, distance));
-	}
-	
-	IEnumerator EffectRaise(float set, float distance, int mat)
+    [Flags]
+    public enum conditions
     {
-		yield return new WaitForSeconds(distance / 10f);
-
-		if(rend.materials[mat].HasFloat("_EffectSize")){
-			float size = rend.materials[mat].GetFloat("_EffectSize");
-
-			while (Mathf.Abs(size - set) > .01f)
-			{
-
-				rend.materials[mat].SetFloat("_EffectSize", size + (set - size) * .05f);
-				size = rend.materials[mat].GetFloat("_EffectSize");
-
-				yield return new WaitForSeconds(.01f);
-			}
-		}
-
-		if (set >= 0) UpdateEffects(false, distance);
+        None  = 0,
+        Black = 1 << 0,
+        White = 1 << 1,
+        All   = Black | White
     }
 
-	IEnumerator HighlightRaise(float set, float distance)
-	{
-		yield return new WaitForSeconds(distance / 10f);
-		rend.gameObject.layer = LayerMask.NameToLayer("Highlight");
+    public conditions obstructed;
+    public bool isWhite;
+    private Coroutine raise;
 
-		var mpb = new MaterialPropertyBlock();
-		rend.GetPropertyBlock(mpb);
+    void Awake() => rend = GetComponentInChildren<Renderer>();
 
+    public IEnumerable<ChessPiece> GetAllOccupants() => tileOccupants;
 
-		float size = mpb.GetFloat("_Size");
+    public void SwapColor(bool white)
+    {
+        rend.sharedMaterial = white
+            ? ChessBoard2.Instance.WhiteTileMat
+            : ChessBoard2.Instance.BlackTileMat;
 
-		while (Mathf.Abs(size - set) > .01f)
-		{
-			mpb.SetFloat("_Size", size + (set - size) * .05f);
-			rend.SetPropertyBlock(mpb);
-			size = mpb.GetFloat("_Size");
-
-			yield return new WaitForSeconds(.01f);
-		}
-
-		mpb.SetFloat("_Size", set);
-		rend.SetPropertyBlock(mpb);
-
-		if(set >= 0) rend.gameObject.layer = LayerMask.NameToLayer("Tile");
-
+        isWhite = white;
     }
 
+    public void AddPiece(ChessPiece piece)
+    {
+        tileOccupants.Add(piece);
+        piece.ScaleLongestAxis();
+        UpdatePieces();
+        UpdateEffects();
+    }
+
+    public void RemovePiece(ChessPiece piece)
+    {
+        if (piece == null) return;
+        tileOccupants.Remove(piece);
+        UpdatePieces();
+        UpdateEffects();
+    }
+
+    public void UpdatePieces()
+    {
+        foreach (var p in tileOccupants)
+            p.UpdatePieceData(this);
+    }
+
+    public void AddEffect(string name, int duration, float distance = 0f)
+    {
+        effects[name] = effects.TryGetValue(name, out var d)
+            ? Mathf.Max(d, duration)
+            : duration;
+
+        UpdateEffects(false, distance);
+        StartCoroutine(EffectRaise(-0.3f, distance, effects.Count));
+    }
+
+    public void UpdateEffects(bool turnPass = true, float distance = 0f)
+    {
+        // ---- decrement safely ----
+        if (turnPass)
+        {
+            var keys = effects.Keys.ToList();
+            foreach (var k in keys)
+            {
+                effects[k]--;
+                if (effects[k] <= 0)
+                    effects.Remove(k);
+            }
+        }
+
+        // ---- rebuild materials cleanly ----
+        var mats = new List<Material> { rend.materials[0] };
+
+        foreach (var kvp in effects)
+        {
+            var mat = effectDictionary.FirstOrDefault(e => e.key == kvp.Key)?.value;
+            if (mat != null)
+                mats.Add(mat);
+        }
+
+        rend.materials = mats.ToArray();
+
+        // ---- reset and recalc obstruction flags ----
+        obstructed = conditions.None;
+
+        if (tileOccupants.Count > 0 || effects.ContainsKey("water"))
+            obstructed |= conditions.All;
+
+        if (effects.ContainsKey("scarewhite"))
+            obstructed |= conditions.White;
+
+        if (effects.ContainsKey("scareblack"))
+            obstructed |= conditions.Black;
+    }
+
+    public void Highlight(float distance)
+    {
+        if (raise != null) StopCoroutine(raise);
+        raise = StartCoroutine(HighlightRaise(-0.5f, distance));
+    }
+
+    public void UnHighlight(float distance)
+    {
+        if (raise != null) StopCoroutine(raise);
+        raise = StartCoroutine(HighlightRaise(0f, distance));
+    }
+
+    IEnumerator EffectRaise(float target, float distance, int matIndex)
+    {
+        yield return new WaitForSeconds(distance / 10f);
+
+        if (matIndex >= rend.materials.Length)
+            yield break;
+
+        var mat = rend.materials[matIndex];
+        if (!mat.HasFloat("_EffectSize"))
+            yield break;
+
+        float size = mat.GetFloat("_EffectSize");
+
+        while (Mathf.Abs(size - target) > 0.01f)
+        {
+            size = Mathf.Lerp(size, target, 0.05f);
+            mat.SetFloat("_EffectSize", size);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    IEnumerator HighlightRaise(float target, float distance)
+    {
+        yield return new WaitForSeconds(distance / 10f);
+
+        rend.gameObject.layer = LayerMask.NameToLayer("Highlight");
+
+        var mpb = new MaterialPropertyBlock();
+        rend.GetPropertyBlock(mpb);
+
+        float size = mpb.GetFloat("_Size");
+
+        while (Mathf.Abs(size - target) > 0.01f)
+        {
+            size = Mathf.Lerp(size, target, 0.05f);
+            mpb.SetFloat("_Size", size);
+            rend.SetPropertyBlock(mpb);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        mpb.SetFloat("_Size", target);
+        rend.SetPropertyBlock(mpb);
+
+        if (target >= 0)
+            rend.gameObject.layer = LayerMask.NameToLayer("Tile");
+    }
 }
