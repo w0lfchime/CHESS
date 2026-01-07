5575fb9892885087be9a131d5bc364d534d9fb80
frogggggggg <139102704+frogggggggg@users.noreply.github.com>
2026-01-07 04:57:52 -0500

    added tutorial and some other stuff

--- Assets/Tutorial.cs
+++ Assets/Tutorial.cs
@@ -1,32 +1,50 @@
 using UnityEngine;
 using System.Collections.Generic;
-using UnityEditor;
+using TMPro;
+using UnityEngine.EventSystems;
+using Unity.VisualScripting;
 
+[System.Serializable]
+public class TutorialPiece
+{
+    public string text;
+    public Transform point;
+    public float timeAmount = -1;
+}
 public class Tutorial : MonoBehaviour
 {
-    public List<Vector3> positions = new List<Vector3>();
-}
+    public CanvasGroup uiGroup;
+    public bool running = true;
+    public List<TutorialPiece> tutorialPiece = new List<TutorialPiece>();
+    public float speed;
+    public TMP_Text text;
+    private int step = 0;
 
-
-#if UNITY_EDITOR
-
-[CustomEditor(typeof(Tutorial))]
-public class PositionRecorderEditor : Editor
-{
-    public override void OnInspectorGUI()
+    void Update()
     {
-        DrawDefaultInspector();
+        if(!running) return;
 
-        Tutorial recorder = (Tutorial)target;
+        transform.position+=(tutorialPiece[step].point.position-transform.position)*Time.deltaTime*speed;
+        transform.eulerAngles+=(tutorialPiece[step].point.eulerAngles-transform.eulerAngles)*Time.deltaTime*speed;
+        transform.localScale+=(tutorialPiece[step].point.localScale-transform.localScale)*Time.deltaTime*speed;
+        text.text = tutorialPiece[step].text;
 
-        GUILayout.Space(10);
+        if(tutorialPiece[step].timeAmount!=-1) {
+            uiGroup.interactable = false;
+            tutorialPiece[step].timeAmount-=Time.deltaTime;
+            if(tutorialPiece[step].timeAmount<=0) NextStep();
+        }else uiGroup.interactable = true;
+    }
 
-        if (GUILayout.Button("Add Current Position"))
-        {
-            Undo.RecordObject(recorder, "Add Position");
-            recorder.positions.Add(recorder.transform.position);
-            EditorUtility.SetDirty(recorder);
-        }
+    public void StartTutorial()
+    {
+        running = true;
+    }
+
+    public void NextStep()
+    {
+        if(tutorialPiece.Count-1 == step) running = false;
+        if(!running) return;
+        step++;
     }
 }
-#endif
